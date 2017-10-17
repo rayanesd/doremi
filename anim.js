2353b9dd689b0cc26715af95da866f1b3a7ba548
rayanesd <32707159+rayanesd@users.noreply.github.com>
2017-10-17 17:02:54 +0200

    Journée 2.2
    Liste complétée, logo ajouté, design

--- anim.js
+++ anim.js
@@ -1,26 +1,8 @@
 $(document).ready(function(){
-  $("#menu1").mouseenter(function(){
+  $(".menu1").mouseenter(function(){
     $(this).fadeTo("fast", 0.30);
   });
-  $("#menu1").mouseleave(function(){
-    $(this).fadeTo("fast", 100);
-  });
-  $("#menu2").mouseenter(function(){
-    $(this).fadeTo("fast", 0.30);
-  });
-  $("#menu2").mouseleave(function(){
-    $(this).fadeTo("fast", 100);
-  });
-  $("#menu3").mouseenter(function(){
-    $(this).fadeTo("fast", 0.30);
-  });
-  $("#menu3").mouseleave(function(){
-    $(this).fadeTo("fast", 100);
-  });
-  $("#menu4").mouseenter(function(){
-    $(this).fadeTo("fast", 0.30);
-  });
-  $("#menu4").mouseleave(function(){
+  $(".menu1").mouseleave(function(){
     $(this).fadeTo("fast", 100);
   });
 });
