f28d3358dfe6c126b2625ded01acf389b9a1ee52
rayanesd <32707159+rayanesd@users.noreply.github.com>
2017-10-17 11:50:27 +0200

    Journée 2.1
    Première mise à jour avec des ajouts d'animations JQuery

---
+++ anim.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $("#menu1").mouseenter(function(){
+    $(this).fadeTo("fast", 0.30);
+  });
+  $("#menu1").mouseleave(function(){
+    $(this).fadeTo("fast", 100);
+  });
+  $("#menu2").mouseenter(function(){
+    $(this).fadeTo("fast", 0.30);
+  });
+  $("#menu2").mouseleave(function(){
+    $(this).fadeTo("fast", 100);
+  });
+  $("#menu3").mouseenter(function(){
+    $(this).fadeTo("fast", 0.30);
+  });
+  $("#menu3").mouseleave(function(){
+    $(this).fadeTo("fast", 100);
+  });
+  $("#menu4").mouseenter(function(){
+    $(this).fadeTo("fast", 0.30);
+  });
+  $("#menu4").mouseleave(function(){
+    $(this).fadeTo("fast", 100);
+  });
+});
